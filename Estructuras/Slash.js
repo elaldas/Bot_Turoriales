14a0fc0580effd84c864d7ac284920966fd80de8
ELALDA <122388477+elaldas@users.noreply.github.com>
2024-05-25 16:20:37 -0500

    Add files via upload

---
+++ Estructuras/Slash.js
@@ -0,0 +1,18 @@
+const { EmbedBuilder } = require("discord.js");//Requerimos los paquetes
+
+module.exports = {
+	name: "",//Nombre del comando
+	description: "",//Descripción del comando
+
+	async execute(client, interaction){
+
+		interaction.reply({})
+	}
+}
+
+/*
+╔═══════════════════════════════════════════════════╗
+║  || - ||   Código por ALDA#8939/el_alda   || - |  ║
+║     --|   https://discord.gg/JpKGJFZCzK    |--    ║
+╚═══════════════════════════════════════════════════╝
+*/
