34c43c969d15ea185cf2e8568e482101aefc3cd8
ELALDA <122388477+elaldas@users.noreply.github.com>
2025-01-20 18:54:44 -0500

    Add files via upload

---
+++ Schemas/Configuraciones/Tickets/TicketsSetup.js
@@ -0,0 +1,23 @@
+const { model, Schema } = require("mongoose")
+
+const setup = new Schema({
+
+	ServerID: {
+		type: String,
+		required: true
+	},
+	CanalID: {
+		type: String,
+		required: true
+	},
+	CategoriaID: {
+		type: String,
+		required: true
+	},
+	RolID: {
+		type: String,
+		required: true
+	}
+})
+
+module.exports = model("TicketsSetup", setup)
---
+++ Schemas/Configuraciones/Tickets/TicketsSetupCanales.js
@@ -0,0 +1,23 @@
+const { model, Schema } = require("mongoose")
+
+const canales = new Schema({
+
+	ServerID: {
+		type: String,
+		required: true
+	},
+	Canal: {
+		type: String,
+		required: true
+	},
+	AuthorID: {
+		type: String,
+		required: true
+	},
+	Cerrado: {
+		type: String,
+		required: true
+	}
+})
+
+module.exports = model("TicketsSetupCanales", canales)
